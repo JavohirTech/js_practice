6c5cfff9fdf274b0d89fa1c3b83363f3bff464bb
javohirtech <javohirtech@gmail.com>
2023-04-24 13:26:16 +0500

    bug fixed and made beautiful design than previous commit

--- js_animatedNotification/script.js
+++ js_animatedNotification/script.js
@@ -1,21 +1,45 @@
-let successBtn = document.querySelector('.success-btn');
-let errorBtn = document.querySelector('.error-btn');
-let warningBtn = document.querySelector('.warning-btn');
-let infoBtn = document.querySelector('.info-btn');
-let alert = document.querySelector('.alert');
+const notifications = document.querySelector(".notifications"),
+    buttons = document.querySelectorAll(".buttons .btn");
 
-let buttons = document.querySelectorAll('.buttons');
+const toastDetails = {
+    timer: 5000,
+    success: {
+        icon: 'fa-circle-check',
+        text: 'Success: This is a success notification.',
+    },
+    error: {
+        icon: 'fa-circle-xmark',
+        text: 'Error: This is an error notification.',
+    },
+    warning: {
+        icon: 'fa-triangle-exclamation',
+        text: 'Warning: This is a warning notification.',
+    },
+    info: {
+        icon: 'fa-circle-info',
+        text: 'Info: This is an information notification.',
+    }
+}
 
-buttons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        alert.classList.remove('hide');
-        alert.classList.add('show');
-        console.log(alert);
-        setTimeout(() => {
-            alert.classList.remove('show');
-            alert.classList.add('hide');
-            console.log(alert);
-        }, 3000)
-    })
+const removeToast = (toast) => {
+    toast.classList.add("hide");
+    if (toast.timeoutId) clearTimeout(toast.timeoutId);
+    setTimeout(() => toast.remove(), 500);
+}
 
-})
+const createToast = (id) => {
+    const { icon, text } = toastDetails[id];
+    const toast = document.createElement("li");
+    toast.className = `toast ${id}`;
+    toast.innerHTML = `<div class="column">
+                         <i class="fa-solid ${icon}"></i>
+                         <span>${text}</span>
+                      </div>
+                      <i class="fa-solid fa-xmark" onclick="removeToast(this.parentElement)"></i>`;
+    notifications.appendChild(toast);
+    toast.timeoutId = setTimeout(() => removeToast(toast), toastDetails.timer);
+}
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => createToast(btn.id));
+});
